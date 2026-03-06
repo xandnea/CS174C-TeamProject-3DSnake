edbcc839bf56a566ad1c9ee111c18591c279d2cb
Jay <109173281+jayh23@users.noreply.github.com>
2026-03-06 12:21:49 -0800

    Update collect.js

--- collect.js
+++ collect.js
@@ -55,7 +55,7 @@ export const Collectible = class Collectible {
         i = 0; // Reusing the same iterator. Who cares
         while (i < this.instances.length) {
             const c = this.instances[i];
-            if ((hx - c[0]) ** 2 + (hz - c[2]) ** 2 < this.size) {
+            if ((hx - c[0]) ** 2 + (hz - c[2]) ** 2 < this.size + 0.2) {
                 this.particles.push(new Sparkle(c[0], c[1], c[2]));
                 this.instances.splice(i, 1);
                 this.score += 1;
@@ -64,11 +64,10 @@ export const Collectible = class Collectible {
                 i++;
             }
         }
-        const y = Math.sin(t) * 0.25 + 0.35;
+        const y = Math.sin(t) * 0.25 + 0.35 + this.r;
         for (let i of this.instances) {
             i[1] = y;
         }
-        console.log("hi");
     }
 
     draw(webgl_manager, uniforms, shapes, materials) {
